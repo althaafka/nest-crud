25144fc5695940cda41bd2f7c33179e99c82b688
althaafka <althaafka004@gmail.com>
2024-10-12 14:55:25 +0700

    feat: post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -24,7 +25,8 @@ import { AuthModule } from './auth/auth.module';
       }),
     }),
     UserModule,
-    AuthModule
+    AuthModule,
+    PostModule
   ],
 })
 export class AppModule {}
