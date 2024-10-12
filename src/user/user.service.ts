0184e3c47b41cbd5f49f7a7111b8517becd1e8f2
althaafka <althaafka004@gmail.com>
2024-10-12 10:15:06 +0700

    feat: user module

---
+++ src/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  // Get all users
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  // Get user by username
+  async findOneByUsername(username: string): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { username } });
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+}
